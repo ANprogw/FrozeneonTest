a4929a188d34a6683cf1570b52d89843a807ecc8
Anton Novosvitniy <anthonynovos@gmail.com>
2021-09-22 03:47:55 +0300

    final update

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
+            tabContent.classList.remove('shop__shopping-area_status_active')
         })
         tabs.forEach(tab => {
-            tab.classList.remove('active')
+            tab.classList.remove('shop__tab_status_active')
         })
-        tab.classList.add('active')
-        target.classList.add('active')
+        tab.classList.add('shop__tab_status_active')
+        target.classList.add('shop__shopping-area_status_active')
     })
 })
